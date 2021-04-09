425d1afe3aff3b0ae7008bfb7ff1291b1f9c12fa
BeetMacol <beetmacol@gmail.com>
2021-04-09 17:48:29 +0200

    Add initial project structure

---
+++ src/main/java/com/beetmacol/mc/modinfocmd/ModInfoCmd.java
@@ -0,0 +1,12 @@
+package com.beetmacol.mc.modinfocmd;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ModInfoCmd {
+	public static final String MOD_ID = "mod-info-cmd";
+	public static final Logger LOGGER = LogManager.getLogger("Mod Info Cmd");
+
+	public static void init() {
+	}
+}
